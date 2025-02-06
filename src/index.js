84cb9da81ced63f06168019cd90080e8d15103a5
Thor <thorlhansen@hotmail.com>
2025-02-06 01:23:47 +0100

    Refactor project structure: remove unused main.js, update index.js for Firebase uploads, and add scheduled GitHub Actions workflow

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const catalogRoutes = require('./routes/catalogRoutes');
-const config = require('./config/config');
 const CatalogController = require('./controllers/catalogController');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api', catalogRoutes);
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// needs to be put into main and run only when needed ex once a day or when some offers are outdated
 
 const catalogController = new CatalogController();
 
 async function main() {
-    // ...existing code to fetch catalog IDs and write to file...
     // used to upload to firebase, needs a function to only upload when needed
-    //await catalogController.uploadToFirebase('offers.json', offers);
-
-
+    await catalogController.uploadToFirebase('offers.json', 'offers');
+    console.log("Uploading to firebase");
 
     // only needs to run once or if new data is added (hardcoded data)
-    //await catalogController.uploadToFirebase('Foodcompents.json', "foodcompents");
+    await catalogController.uploadToFirebase('Foodcompents.json', "foodcompents");
 }
 
 main().catch(console.error);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
