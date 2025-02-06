a18ed752a16c61e38ff3ca46187fcdec36b6662f
Thor <thorlhansen@hotmail.com>
2025-02-06 02:09:37 +0100

    trying to get github secrets to work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const CatalogController = require('./controllers/catalogController');
 
 const catalogController = new CatalogController();
+// Load environment variables from process.env
+const apiKey = process.env.API_KEY;
+const trackId = process.env.TRACK_ID;
+const businessIds = process.env.BUSINESS_IDS;
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 async function main() {
+    console.log("API_KEY:", apiKey);
+    console.log("TRACK_ID:", trackId);
+    console.log("BUSINESS_IDS:", businessIds);
+    console.log("FIREBASE_API_KEY:", firebaseConfig.apiKey);
+    
     try {
         // Check if a specific condition is met before uploading to Firebase
         const shouldUpload = true; // Replace this with your condition
--- src/routes/catalogRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CatalogController = require('../controllers/catalogController');
-
-const catalogController = new CatalogController();
-
-router.get('/catalogs', (req, res) => catalogController.getCatalogIds(req, res));
-
-module.exports = router;
